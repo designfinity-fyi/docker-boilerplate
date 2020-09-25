daeea1dff746ed33c90ac84ac921235bcd818f09
Domain Manager <admin@openplaytech.com>
2020-09-25 15:38:31 +0530

    Updates

---
+++ precache-manifest.0e5ad51168875c886801dfc14e940da0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "155a0096ad620be8de48b835681edbff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19d2a4f3e6af8fcb7a8d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "92ba63a23e600354fee5",
+    "url": "/static/js/2.de8e7690.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.de8e7690.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19d2a4f3e6af8fcb7a8d",
+    "url": "/static/js/main.4fec2dc2.chunk.js"
+  },
+  {
+    "revision": "ef9482a33432ddc2aee3",
+    "url": "/static/js/runtime-main.85514b1d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
